c6bea9236fa6057d11a8b0c2f8c393644ded3639
Laura_Vrkmp <laura.veerkamp11@gmail.com>
2023-11-24 01:08:33 +0100

    King first three chapters

---
+++ king/Chapter 2/PP 2.2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define M_PI 3.14159265358979323846
+
+int main(void)
+{
+    float rad = 10;
+    float v = 4.0f / 3.0f * M_PI * rad * rad * rad;
+
+    printf("The volume is %.3f.\n", v);
+    return 0;
+}
